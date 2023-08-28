f8b81ae147e4b7cbf5452be00071eabb1c51d4b5
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-08-28 18:20:49 +0000

    Deploying to gh-pages from @ yaromatv/goit-react-hw-05-movies@3e8d90ca5246546fc50d8765b10c6fb70fb700ec 🚀

---
+++ static/js/520.8ab9098f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,r){r.r(t);var c=r(87),n=r(184);t.default=function(){return(0,n.jsxs)("main",{children:[(0,n.jsx)("h2",{children:"404"}),"Go back to ",(0,n.jsx)(c.rU,{to:"/",children:"Home"})]})}}}]);
+//# sourceMappingURL=520.8ab9098f.chunk.js.map
